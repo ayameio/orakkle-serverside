d67662bcc7617fe2b7b1af413393507f0269c6ff
Adrian <adriangaspar@protonmail.com>
2023-04-04 13:08:25 +0200

    commented demo run() in main()

--- src/main/java/dev/ayameio/orakkle/backend/Application.java
+++ src/main/java/dev/ayameio/orakkle/backend/Application.java
@@ -1,22 +1,16 @@
 package dev.ayameio.orakkle.backend;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Optional;
 
 @SpringBootApplication
 public class Application {
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
+//	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String... args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+/*
 	@Bean
 	public CommandLineRunner run(SignalRepository repository) {
 		return (args) -> {
@@ -91,4 +85,5 @@ public class Application {
 			});
 		};
 	}
+*/
 }
